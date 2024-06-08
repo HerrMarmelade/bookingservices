2f9513ab105924abddd8322e427d25a905139e58
HerrMarmelade <alinagi3d@gmail.com>
2024-06-08 16:05:07 +0200

    a

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Start a booking service for your business! Coming Soon!
         </p>
         <a
           className="App-link"
